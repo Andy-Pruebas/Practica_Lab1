6e46b1397927139b69f4bd9f20d0ef28fbb6260c
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-04-23 15:46:24 -0500

    Tercer Commit

--- conex.php
+++ conex.php
@@ -1,7 +1,7 @@
 <?php
 function conectar()
 {
-    if (!($conn =new  mysqli('l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', '	n877zmeh7sw73tpj','h33j8hd6l66i1woh','lwehqrjlb0w2nsac'))) {
+    if (!($conn =new  mysqli('l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'n877zmeh7sw73tpj','h33j8hd6l66i1woh','lwehqrjlb0w2nsac'))) {
         echo "Error al conectarse";
         exit();
     }
