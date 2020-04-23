b1cc43b0b700af2edc48051d7472e16a4e887339
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-04-23 15:51:19 -0500

    Cuarto Commit

--- validacion.php
+++ validacion.php
@@ -7,7 +7,7 @@ if(empty($usua) || empty($clave)){
 	exit();
 }
 
-$conexion = new mysqli('l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'n877zmeh7sw73tpj','h33j8hd6l66i1woh','lwehqrjlb0w2nsac')
+$conexion = new mysqli('l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'n877zmeh7sw73tpj','h33j8hd6l66i1woh','lwehqrjlb0w2nsac');
 
 
 $result = $conexion->query("SELECT * from empleado where user='".$usua."'");
